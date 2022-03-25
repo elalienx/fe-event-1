abc20c2f035638476e7ebcf85bc1ede7b4caec33
Eduardo <el_alienx@hotmail.com>
2022-03-25 16:06:22 +0100

    added firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 // Project files
+import placeholderImage from "assets/images/placeholder.jpg";
+import about1 from "assets/images/about1.jpg";
+import about2 from "assets/images/about2.jpg";
+import about3 from "assets/images/about3.jpg";
+import about4 from "assets/images/about4.jpg";
 import NavigationBar from "components/NavigationBar";
 import english from "data/english.json";
 import "styles/style.sass";
-import placeholderImage from "assets/images/placeholder.jpg";
 
 export default function App() {
   const { header, body, footer } = english;
@@ -16,42 +20,48 @@ export default function App() {
       <NavigationBar />
       <header className="hero">
         <div className="content-center">
-          <h1 className="title">{header.title}</h1>
-          <div className="math-symbols">+</div>
-          <span className="subtitle">{header.subtitle}</span>
-          <div className="math-symbols">=</div>
-          <span className="date">{header.date}</span>
+          <h1 className="title">
+            {header.title}
+            <br />
+            +
+            <br />
+            {header.subtitle}
+            <br />
+            =
+            <br />
+            {header.date}
+          </h1>
         </div>
       </header>
       <section className="about container">
         <h2>About the event</h2>
         {/* Part 1 */}
-        <article className="item-information">
-          <img src={placeholderImage} alt="" />
+        <article className="about-item">
+          <img src={about1} alt="" />
           <div className="content">
             <p>{body.part1}</p>
           </div>
         </article>
 
         {/* Part 2 */}
-        <article className="item-information">
-          <img src={placeholderImage} alt="" />
+        <article className="about-item">
+          <img src={about2} alt="" />
           <div className="content">
             <p>{body.part2}</p>
           </div>
         </article>
 
         {/* Part 3 */}
-        <article className="item-information">
-          <img src={placeholderImage} alt="" />
+        <article className="about-item">
+          <img src={about3} alt="" />
           <div className="content">
             <p>{body.part3}</p>
           </div>
         </article>
 
         {/* Part 4 */}
-        <article className="item-information">
-          <img src={placeholderImage} alt="" />
+        <article className="about-item">
+          <img src={about4} alt="" />
           <div className="content">
             <h3>{body.part4.title}</h3>
             <ul>
@@ -64,16 +74,14 @@ export default function App() {
       </section>
       <section className="call-to-action">
         <div className="content-center">
-          <p>Book your ticket now!</p>
+          <h2>Book your ticket now!</h2>
           <a href={eventLink} className="button">
             Register now
           </a>
         </div>
       </section>
       <footer className="footer">
-        <div className="container">
-          <p>{footer}</p>
-        </div>
+        <div className="container">{footer}</div>
       </footer>
     </div>
   );
