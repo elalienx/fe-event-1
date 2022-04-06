0f5d60e98ff7f69ef28a64a28476601e103c1c7b
Eduardo <el_alienx@hotmail.com>
2022-04-06 12:57:55 +0200

    adding translation button

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,7 +1,21 @@
 // Project files
 import Logo from "assets/images/logo-white.png";
+import { useState } from "react";
 
 export default function NavigationBar({ item }) {
+  // Local state
+  const [language, setLanguage] = useState("en");
+
+  // Properties
+  const LanguageLabel = language === "en" ? "English" : "Swedish";
+
+  // Methods
+  function onLanguage() {
+    if (language === "en") setLanguage("sv");
+    else setLanguage("en");
+  }
+
+  // Components
   const Links = item.map((item, index) => (
     <a key={index} target="_blank" href={item.url}>
       {item.name}
@@ -14,7 +28,10 @@ export default function NavigationBar({ item }) {
       <div className="container">
         <img src={Logo} alt="Novare logo with a handwritting overlay" />
         <span className="spacer">{/* empty on purpose */}</span>
-        {Links}
+        <div>{Links}</div>
+        <button className="button" onClick={onLanguage}>
+          {LanguageLabel}
+        </button>
       </div>
     </nav>
   );
